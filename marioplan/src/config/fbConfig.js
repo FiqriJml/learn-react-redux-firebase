20147af0d518b2987f478b40afd3c5014cf251eb
fiqrijml <jamalfiqri@gmail.com>
2020-05-11 15:45:35 +0800

    #19 - Syncing Data with Firestore

--- marioplan/src/config/fbConfig.js
+++ marioplan/src/config/fbConfig.js
@@ -1,9 +1,9 @@
-  import firebase from 'firebase/app'
-  import 'firebase/firestore'
-  import 'firebase/auth'
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
 
   // Your web app's Firebase configuration
-  var firebaseConfig = {
+ export var fbConfig = {
     apiKey: "AIzaSyAOQSGpWwdi-7go6ByYk4r_0R-hlf80Df0",
     authDomain: "vuejs-playlist-ffac2.firebaseapp.com",
     databaseURL: "https://vuejs-playlist-ffac2.firebaseio.com",
@@ -13,6 +13,7 @@
     appId: "1:287538032929:web:8bfdac2dde292b104f0e60"
   };
   // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+  firebase.initializeApp(fbConfig)
+  firebase.firestore()
 
   export default firebase;
--- marioplan/src/index.js
+++ marioplan/src/index.js
@@ -7,22 +7,33 @@ import { createStore, applyMiddleware, compose } from 'redux'
 import rootReducer from './store/reducers/rootReducer';
 import {Provider} from 'react-redux';
 import thunk from 'redux-thunk';
-import { reduxFirestore, getFirestore } from 'redux-firestore';
-import { getFirebase } from 'react-redux-firebase';
-import fbConfig from './config/fbConfig'
+
+import { reduxFirestore, createFirestoreInstance, getFirestore } from 'redux-firestore'
+
+import { ReactReduxFirebaseProvider, getFirebase } from 'react-redux-firebase'
+
+import firebase, {fbConfig} from './config/fbConfig'
 
 const store = createStore(rootReducer, 
   compose(
     applyMiddleware(thunk.withExtraArgument({getFirebase, getFirestore})),
-    reduxFirestore(fbConfig)
+    reduxFirestore(firebase, fbConfig)
     )
 );
+
+const rrfProps = {
+  firebase,
+  config: fbConfig,
+  dispatch: store.dispatch,
+  createFirestoreInstance
+};
+
 ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-    <App />
-    </Provider>
-  </React.StrictMode>,
+  <Provider store={store}>
+    <ReactReduxFirebaseProvider {...rrfProps}>
+      <App />
+    </ReactReduxFirebaseProvider>
+  </Provider>,
   document.getElementById('root')
 );
 
