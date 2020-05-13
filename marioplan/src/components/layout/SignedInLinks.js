91dbf5625fdab308f46e336bbf96e5b1d96fd2db
fiqrijml <jamalfiqri@gmail.com>
2020-05-13 12:49:40 +0800

    #24 - Logging Users Out

--- marioplan/src/components/layout/SignedInLinks.js
+++ marioplan/src/components/layout/SignedInLinks.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { signOut} from '../../store/actions/authActions'
 
-const SignedInLinks = () => {
+const SignedInLinks = (props) => {
     return (
         <ul className="right">
             <li><NavLink to="/create">New Project</NavLink></li>
-            <li><NavLink to="/">Log Out</NavLink></li>
+            <li><NavLink to="#" onClick={props.signOut}>Log Out</NavLink></li>
             <li><NavLink to="/" className="btn btn-floating pink lighten-1">NN</NavLink></li>
         </ul>
     )
 }
+const mapToDispatchProps = (dispatch) =>{
+    return {
+        signOut: () => dispatch(signOut())
+    }
+}
 
-export default SignedInLinks;
+export default connect(null,mapToDispatchProps)(SignedInLinks);
