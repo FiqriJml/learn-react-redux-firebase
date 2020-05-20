e9b66d547c5b46336f9736f051b37966df70cb7a
fiqrijml <jamalfiqri@gmail.com>
2020-05-20 17:49:35 +0800

    #35 - Notification Component

--- marioplan/src/components/dashboard/Dashboard.js
+++ marioplan/src/components/dashboard/Dashboard.js
@@ -17,7 +17,7 @@ class Dashboard extends Component{
                     <div className="col s12 m6">
                         <ProjectList projects={projects}/>
                     </div>
-                    <div className="s12 m5 offset-m1">
+                    <div className="col s12 m5 offset-m1">
                         <Notifications/>
                     </div>
                 </div>
