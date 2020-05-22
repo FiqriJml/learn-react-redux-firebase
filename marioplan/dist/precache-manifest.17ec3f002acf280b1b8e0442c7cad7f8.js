834db79802da27c7711a9e47dd4fb89704035790
fiqrijml <jamalfiqri@gmail.com>
2020-05-22 17:14:34 +0800

    #40 - Deploying to Firebase Hosting

---
+++ marioplan/dist/precache-manifest.17ec3f002acf280b1b8e0442c7cad7f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8eefc51c12fda8c43a1e8708cb2af42",
+    "url": "/index.html"
+  },
+  {
+    "revision": "28e5b2729cb63aca9335",
+    "url": "/static/css/main.cdfd01aa.chunk.css"
+  },
+  {
+    "revision": "7528a22e7a54a3a60956",
+    "url": "/static/js/2.93ed5942.chunk.js"
+  },
+  {
+    "revision": "f9b3e87900b5f3843f2b97c9b5b24d1e",
+    "url": "/static/js/2.93ed5942.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28e5b2729cb63aca9335",
+    "url": "/static/js/main.d8fdbc1d.chunk.js"
+  },
+  {
+    "revision": "9d6581e9a8f0b2cb6b17",
+    "url": "/static/js/runtime-main.88dc66ec.js"
+  }
+]);
